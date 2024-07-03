6f266ebe5d0b8ac06c02198b7aac3d3ba5f70f9c
MaxBarneto <mabarneto@frba.utn.edu.ar>
2024-07-03 16:48:06 -0300

    avance con nodos de datos

--- lib/datos/dato_dynamic_supervisor.ex
+++ lib/datos/dato_dynamic_supervisor.ex
@@ -1,22 +1,34 @@
-defmodule DatoDynamicSupervisor do
-  use DynamicSupervisor
+defmodule Datos.DynamicSupervisor do
+  use Horde.DynamicSupervisor
 
   def start_link(init_arg) do
-    DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+    Horde.DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
-  def init(_init_arg) do
-    DynamicSupervisor.init(strategy: :one_for_one, max_restarts: 5, max_seconds: 5)
+  def init(init_arg) do
+    [
+      members: members(),
+      strategy: :one_for_one,
+      distribution_strategy: Horde.UniformQuorumDistribution,
+      process_redistribution: :active
+    ]
+    |> Keyword.merge(init_arg)
+    |> Horde.DynamicSupervisor.init()
+  end
+
+  defp members do
+    Enum.map(Node.list([:this, :visible]), &{__MODULE__, &1})
   end
 
   def start_child(child_name, state) do
     spec = {Dato, {child_name, state}}
-    DynamicSupervisor.start_child(__MODULE__, spec)
+    Horde.DynamicSupervisor.start_child(__MODULE__, spec)
+    {:ok, child_name}
   end
 end
 
 # Examples
-# {:ok, pid_1} = DatoDynamicSupervisor.start_child(Nodo1, [])
+# {:ok, pid_1} = Datos.DynamicSupervisor.start_child(Nodo1, [])
 # Dato.insert(pid_1, :pepe, "pepa")
 # Dato.insert(pid_1, :tp, "1C2024")
 # Dato.get(pid_1, :pepe)
