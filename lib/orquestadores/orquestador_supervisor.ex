eabaef3c8031bdf21d1d796edcd1b5ce74137121
MaxBarneto <mabarneto@frba.utn.edu.ar>
2024-07-12 18:18:19 -0300

    diagrama de arquitectura

---
+++ lib/orquestadores/orquestador_supervisor.ex
@@ -0,0 +1,17 @@
+defmodule OrquestadorSupervisor do 
+    use Supervisor
+
+    def start_link(init) do
+        Supervisor.start_link(__MODULE__, init, name: __MODULE__)
+      end
+    
+      def init(_init_arg) do
+        children = [
+          {Orquestador,[]}
+        ]
+  
+        opts = [strategy: :one_for_one]
+    
+        Supervisor.init(children, opts)
+      end
+end
