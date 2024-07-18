80607d306c70fef6a7582513790cb6b016e5728f
comejia <comejia@teco.com.ar>
2024-07-18 20:42:10 -0300

    Add random port

--- lib/application.ex
+++ lib/application.ex
@@ -8,10 +8,17 @@ defmodule KV.Application do
         config: [hosts: [:"a@127.0.0.1", :"b@127.0.0.1"]]
       ]
     ]
+    #Asegúrate de iniciar el generador de números aleatorios
+    :rand.seed(:exsplus, :os.timestamp())
+
+    #Genera un número aleatorio entre 5000 y 6000
+    numero_aleatorio = 5000 + :rand.uniform(1001) - 1
+
+    IO.puts("Número aleatorio entre 5000 y 6000: #{numero_aleatorio}")
 
     children = [
       {Cluster.Supervisor, [topologies, [name: KV.ClusterSupervisor]]}, #libcluster
-      {Plug.Cowboy, scheme: :http, plug: KVServer, options: [port: 3000]},
+      {Plug.Cowboy, scheme: :http, plug: KVServer, options: [port: numero_aleatorio]},
       #Supervisores
       Datos.Supervisor,
       NodeManager.Supervisor,
