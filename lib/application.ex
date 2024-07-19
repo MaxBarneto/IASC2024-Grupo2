1d401a839bcc1d7871c822145665aaad7aeff5a2
Matias Pardo <pardmatias@frba.utn.edu.ar>
2024-07-18 23:20:06 -0300

    puerto para libcluster

--- lib/application.ex
+++ lib/application.ex
@@ -2,19 +2,25 @@ defmodule KV.Application do
   use Application
 
   def start(_start_type, _start_args) do
+    # Asegúrate de iniciar el generador de números aleatorios
+    :rand.seed(:exsplus, :os.timestamp())
+
+    # Genera un número aleatorio entre 5000 y 6000
+    numero_aleatorio = 5000 + :rand.uniform(1001) - 1
+    numero_aleatorio_cluster = 5000 + :rand.uniform(1001) - 1
+
+    IO.puts("Número aleatorio para Plug.Cowboy entre 5000 y 6000: #{numero_aleatorio}")
+    IO.puts("Número aleatorio para libcluster entre 5000 y 6000: #{numero_aleatorio_cluster}")
+
     topologies = [
       libcluster_strategy: [
         strategy: Cluster.Strategy.Gossip,
-        config: [hosts: [:"a@127.0.0.1", :"b@127.0.0.1"]]
-      ]
+        config:
+        [
+          hosts: [:"a@127.0.0.1", :"b@127.0.0.1"],
+          port: numero_aleatorio_cluster]
+        ]
     ]
-    #Asegúrate de iniciar el generador de números aleatorios
-    :rand.seed(:exsplus, :os.timestamp())
-
-    #Genera un número aleatorio entre 5000 y 6000
-    numero_aleatorio = 5000 + :rand.uniform(1001) - 1
-
-    IO.puts("Número aleatorio entre 5000 y 6000: #{numero_aleatorio}")
 
     children = [
       {Cluster.Supervisor, [topologies, [name: KV.ClusterSupervisor]]}, #libcluster
