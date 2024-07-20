4db9284e09d0f41509655395591b99d4024bf97b
MaxBarneto <mabarneto@frba.utn.edu.ar>
2024-07-20 00:40:06 -0300

    agente recupera su estado desde las replicas

--- lib/datos/dato_agent.ex
+++ lib/datos/dato_agent.ex
@@ -7,10 +7,15 @@ defmodule DatoAgent do
 
   def start_link(initial_state, name, value) do
     {:ok, pid} = Agent.start_link(fn -> initial_state end, name: {:via, Registry, {DatoRegistry, name, value}})
+    DatoAgent.init(pid)
   end
 
-  def init(initial_state) do
-    {:ok, initial_state}
+  def init(pid) do
+    replicated_data = DatoAgent.get_data_from_replicas(pid)
+    if not is_nil(replicated_data) do
+      Agent.update(pid,fn (state) -> Map.merge(state,replicated_data) end) 
+    end
+    {:ok, pid}
   end
 
   def child_spec({state, name, value}) do
@@ -43,6 +48,14 @@ defmodule DatoAgent do
   def data_size(pid) do
     data = DatoAgent.getAll(pid)
     map_size(data)
+  end
+
+  def get_data_from_replicas(pid) do
+    value = DatoRegistry.find_agent_by_pid(pid) |> elem(2)
+    replicas = DatoRegistry.find_replicas_for(value)
+    replicas_pids = Enum.map(replicas, fn {_,x,_} -> x end)
+    replica_data = Enum.map(replicas_pids, fn pid -> DatoAgent.getAll(pid) end)
+    Enum.filter(replica_data, fn map -> map_size(map) > 0 end) |> List.first()
   end
 
 end
