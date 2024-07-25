1fd321dcef0d9611099a8abfcd62d78bda634fb9
comejia <comejia@teco.com.ar>
2024-07-25 20:24:44 -0300

    Update init file

--- lib/init/init.ex
+++ lib/init/init.ex
@@ -1,20 +1,25 @@
 defmodule Init do
 
   def create_orchestrators() do
-    case Node.self() do
-      :"agent_1@127.0.0.1" ->
-        OrquestadorDynamicSupervisor.start_child(:o1, :master)
-      :"agent_2@127.0.0.1" ->
-        OrquestadorDynamicSupervisor.start_child(:o2, :slave)
-      :"replica_1@127.0.0.1" ->
-        OrquestadorDynamicSupervisor.start_child(:o3, :slave)
-        Orquestador.insert("key1", "value1")
-        Orquestador.insert("key2", "value2")
-      :"replica_2@127.0.0.1" ->
-        OrquestadorDynamicSupervisor.start_child(:o4, :slave)
-        Orquestador.insert("key3", "value3")
-        Orquestador.insert("key4", "value4")
-      _ -> IO.puts("No se ha encontrado el nodo")
-    end
+    nodos = [Node.self() | Node.list]
+
+    nodos |> Enum.each(fn node -> 
+      name = for _ <- 1..5, into: "", do: <<Enum.random(to_charlist("abcdef0123456789"))>>
+      :erpc.call(node, OrquestadorDynamicSupervisor,:start_child, [name, :slave])
+    end)
+    
+    {orq_id, _, _} = OrquestadorHordeRegistry.get_any
+    Orquestador.set_as_master(orq_id)
+  end
+
+  def load_data(percentage) do
+    capacity = Application.fetch_env!(:kv, :max_capacity_for_node) * length(NodeManager.agent_node_list()) * percentage
+    IO.puts("Llenando la DB con #{capacity} datos")
+
+    Enum.each(0..trunc(capacity), fn _ ->
+      key = for _ <- 1..3, into: "", do: <<Enum.random(to_charlist("abcde"))>>
+      value = for _ <- 1..5, into: "", do: <<Enum.random(to_charlist("abcde"))>>
+      Orquestador.insert(key, value)
+    end)
   end
 end
